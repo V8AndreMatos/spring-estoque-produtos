7f746875164ab3fba77690ef4f14e3514880ca45
Andre <andreluisv8@gmail.com>
2025-10-01 18:32:30 -0300

    Created class ProductService

--- src/main/java/com/produtos/estoque/repositories/ProductRepository.java
+++ src/main/java/com/produtos/estoque/repositories/ProductRepository.java
@@ -8,6 +8,6 @@ import java.util.UUID;
 
     public interface ProductRepository extends JpaRepository<Product, UUID> {
 
-        
+
 
     }
---
+++ src/main/java/com/produtos/estoque/services/ProductService.java
@@ -0,0 +1,14 @@
+package com.produtos.estoque.services;
+
+import com.produtos.estoque.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+
+}
