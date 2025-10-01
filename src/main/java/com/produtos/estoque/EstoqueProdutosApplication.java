6adb2bf5f96ab0566cf7c90e97913814f17a8ba4
Andre <andreluisv8@gmail.com>
2025-10-01 12:17:55 -0300

    Project created

---
+++ src/main/java/com/produtos/estoque/EstoqueProdutosApplication.java
@@ -0,0 +1,13 @@
+package com.produtos.estoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoqueProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoqueProdutosApplication.class, args);
+	}
+
+}
